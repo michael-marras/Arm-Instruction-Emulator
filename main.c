5cb1729c6b632d75e4d36c8b8bd8e505b1e6278a
Michael Marras <marrasmichael2499@gmail.com>
2026-03-29 13:24:52 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,15 @@
+#include "tools.h"
+
+// Constants
+const int numRegisters = 32;
+
+// Global Variables
+bool gRunning = true;
+int gRegisters[numRegisters];
+
+int main (int argc, char* argv[]) {
+	
+	
+	while (running) {
+	}
+}
