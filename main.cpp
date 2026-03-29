e66b46032eccd1b242e2bcbcd7cde4b6de0bdcd2
Michael Marras <marrasmichael2499@gmail.com>
2026-03-29 15:21:16 -0700

    Update and rename main.c to main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Constants
+
+// Global Variables
+bool gRunning = true;
+
+int main (int argc, char* argv[]) {
+	while (gRunning) {
+		
+		std::cout << "Type Quit! to end" << std::endl;
+		
+	}
+}
